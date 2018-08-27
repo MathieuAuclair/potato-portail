86a2115d64ad0101b5d656f570c5a0e462673358
mathieuauclair <mathiass12@hotmail.com>
2018-08-27 12:24:11 -0400

    add controller entreprise

---
+++ SysInternshipManagement/Controllers/EntrepriseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SysInternshipManagement.Controllers
+{
+    public class EntrepriseController : Controller
+    {
+        // GET: Entreprise
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
