cf86409c62de07e398e86a26a99eaaaf8f3a3e6f
Gabriel Leroux <gab.leroux35@gmail.com>
2018-09-04 09:55:59 -0400

    Interface internship (#6)
    * Update gitignore
    
    * Interface-intership-V1
    
    * Intership interface Update V1
    
    * WIP Test
    
    * Remove Second Solution From Main Solution
    
    * Add Second Solution To The Main Solution
    
    * WIP interface-internship update
    
    Update on 30/08/2018
    
    * Last update intership interface before presentation

---
+++ SysInternshipManagement/Controllers/InternshipController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SysInternshipManagement.Controllers
+{
+    public class InternshipController : Controller
+    {
+        // GET: Intership
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SysInternshipManagement/Scripts/InternshipScript.js
@@ -0,0 +1,10 @@
+﻿/*$(function () {
+    $('btnExpandable').click(function () {
+        $(this).('<img src="~/Content/Icons/baseline-expand_less-24px.svg" />');
+        return false;
+    });
+
+
+
+
+})*/
