66f5734429b39d137d187af559e398481b3e9026
mathieuauclair <mathiass12@hotmail.com>
2018-09-03 23:50:31 -0400

    add props example

---
+++ SysInternshipManagement/Models/Widget/FlatUiTextInputModel.cs
@@ -0,0 +1,12 @@
+namespace SysInternshipManagement.Models.Widget
+{
+    public class FlatUiTextInputModel
+    {
+        public string Placeholder { get; set; }
+
+        public FlatUiTextInputModel(string placeholder)
+        {
+            this.Placeholder = placeholder;
+        }
+    }
+}
