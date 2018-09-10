98f7792d083fc0cb353a3c535327ca88c686e9a8
Gabriel Leroux <gab.leroux35@gmail.com>
2018-09-10 10:17:29 -0400

    start interface navigation application

---
+++ SysInternshipManagement/Controllers/ApplicationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SysInternshipManagement.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
