9797498ac05fd0b14e3973a1216335cfa76019fe
Gabriel Leroux <gab.leroux35@gmail.com>
2018-09-13 14:25:17 -0400

    upload a file to an internship

--- SysInternshipManagement/Controllers/InternshipController.cs
+++ SysInternshipManagement/Controllers/InternshipController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,15 +9,44 @@ namespace SysInternshipManagement.Controllers
 {
     public class InternshipController : Controller
     {
+        public static string fileName;
+
         // GET: Intership
         public ActionResult Edit()
         {
             return View();
-        } 
+        }
+
+        [HttpPost]
+        public ActionResult Edit(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                var Name = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/DescriptionStage"), Name);
+                fileName = Name;
+                file.SaveAs(path);
+            }
+
+            return RedirectToAction("Index");
+        }
+
 
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult UploadFile()
+        {
+            Response.ContentType = "application/octet-stream";
+            Response.AppendHeader("content-disposition", "attachement;filename=" + fileName);
+            Response.TransmitFile(Server.MapPath("~/DescriptionStage/" + fileName));
+            Response.End();
+
+            return View();
+        }
+
+
     }
 }
---
+++ SysInternshipManagement/Models/Widget/FlatUiInputButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SysInternshipManagement.Models.Widget
+{
+    public class FlatUiInputButton
+    {
+        public string InputName { get; set; }
+
+        public FlatUiInputButton(string inputName)
+        {
+            InputName = inputName;
+        }
+    }
+}
