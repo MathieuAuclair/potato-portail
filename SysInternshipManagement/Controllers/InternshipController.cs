fafaf7486fc39fd81f33dab6f5523cbeff14496a
mathieuauclair <mathiass12@hotmail.com>
2018-09-26 13:07:13 -0400

    add update

--- SysInternshipManagement/Controllers/InternshipController.cs
+++ SysInternshipManagement/Controllers/InternshipController.cs
@@ -6,6 +6,8 @@ using SysInternshipManagement.Migrations;
 using System.Web;
 using System.Web.Mvc;
 using SysInternshipManagement.Models;
+using System.Data.Entity;
+using System.Diagnostics;
 
 namespace SysInternshipManagement.Controllers
 {
@@ -17,7 +19,110 @@ namespace SysInternshipManagement.Controllers
         // GET: Intership
         public ActionResult Edit()
         {
-            if (Request.QueryString["create"] != null) {
+            return View(db.internship.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var Name = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/DescriptionStage"), Name);
+                fileName = Name;
+                file.SaveAs(path);
+            }
+
+            if (!isRequestValidForInternshipUpdate())
+            {
+                Response.StatusCode = 400;
+                Response.End();
+            }
+
+            Internship internship = (
+                from entity in db.internship
+                where entity.IdInternship == 1
+                select entity
+                ).First();
+
+            Post post = (
+                from entity
+                in db.post
+                where entity.IdPost == Convert.ToInt32(Request.Form["idPost"])
+                select entity
+                ).First();
+
+            Contact contact = (
+                from entity
+                in db.contact
+                where entity.IdContact == Convert.ToInt32(Request.Form["idContact"])
+                select entity
+                ).First();
+
+            Status status = (
+                from entity
+                in db.status
+                where entity.IdStatus == Convert.ToInt32(Request.Form["idStatus"])
+                select entity
+                ).First();
+
+            Location location = (
+                from entity
+                in db.location
+                where entity.IdLocation == Convert.ToInt32(Request.Form["idLocation"])
+                select entity
+                ).First();
+
+            internship.Post = post;
+            internship.Contact = contact;
+            internship.Status = status;
+            internship.Location = location;
+            internship.Description = Request.Form["Description"];
+            internship.Address = Request.Form["Address"];
+            internship.DocumentName = Request.Form["DocumentName"];
+            internship.PostalCode = Request.Form["PostalCode"];
+            internship.Salary = Convert.ToSingle(Request.Form["Salary"]);
+
+            db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+        private bool isRequestValidForInternshipUpdate()
+        {
+            return (
+                Request.Form["IdPost"] != null &&
+                Request.Form["IdContact"] != null &&
+                Request.Form["IdStatus"] != null &&
+                Request.Form["IdLocation"] != null &&
+                Request.Form["Description"] != null &
+                Request.Form["Address"] != null &&
+                Request.Form["PostalCode"] != null &&
+                Request.Form["Salary"] != null
+                );
+        }
+
+        public ActionResult Index()
+        {
+            return View(db.internship.ToList());
+        }
+
+        public ActionResult UploadFile()
+        {
+            Response.ContentType = "application/octet-stream";
+            Response.AppendHeader("content-disposition", "attachement;filename=" + fileName);
+            Response.TransmitFile(Server.MapPath("~/DescriptionStage/" + fileName));
+            Response.End();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddInternship()
+        {
+            if (Request.Form["create"] != null)
+            {
                 var post = new Post();
                 post.Name = "test ";
                 db.post.Add(post);
@@ -57,42 +162,6 @@ namespace SysInternshipManagement.Controllers
 
                 db.SaveChanges();
             }
-
-            return View(db.internship.ToList());
-        }
-
-        [HttpPost]
-        public ActionResult Edit(HttpPostedFileBase file)
-        {
-            if (file.ContentLength > 0)
-            {
-                var Name = Path.GetFileName(file.FileName);
-                var path = Path.Combine(Server.MapPath("~/DescriptionStage"), Name);
-                fileName = Name;
-                file.SaveAs(path);
-            }
-
-            return RedirectToAction("Index");
-        }
-
-
-        public ActionResult Index()
-        {
-            return View(db.internship.ToList());
-        }
-
-        public ActionResult UploadFile()
-        {
-            Response.ContentType = "application/octet-stream";
-            Response.AppendHeader("content-disposition", "attachement;filename=" + fileName);
-            Response.TransmitFile(Server.MapPath("~/DescriptionStage/" + fileName));
-            Response.End();
-
-            return View();
-        }
-
-        public ActionResult addInternship()
-        {
             return RedirectToAction("Index");
         }
     }
