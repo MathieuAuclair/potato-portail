5d690652fce76990e85fed569c4312087a84d301
MathieuAuclair <mathiass12@hotmail.com>
2018-10-31 13:33:46 -0400

    add feature status

--- SysInternshipManagement/Controllers/StatusController.cs
+++ SysInternshipManagement/Controllers/StatusController.cs
@@ -1,21 +1,72 @@
-using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Web.Mvc;
 using SysInternshipManagement.Migrations;
 using SysInternshipManagement.Models;
 
 namespace SysInternshipManagement.Controllers
 {
-    public class StatusController 
+    public class StatusController : Controller
     {
         private readonly DatabaseContext _bd = new DatabaseContext();
 
-        public IEnumerable<Status> RecolterTousLesStatus()
+        [HttpGet]
+        public ActionResult Index()
         {
-            return (
-                from entity
-                    in _bd.status
-                select entity
-            );
+            return View("~/Views/Status/Index.cshtml", _bd.status.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Edition(int? idStatus)
+        {
+            if (idStatus == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var status = _bd.status.Find(idStatus);
+
+            if (status == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+
+            return View("~/Views/Status/Edition.cshtml", status);
+        }
+
+        [HttpPost]
+        public ActionResult EnregistrerLesModifications(
+            int? idStatus,
+            string nom
+        )
+        {
+            if (idStatus == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var status = _bd.status.Find(idStatus);
+
+            if (status == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+
+            status.StatusStage = nom;
+            _bd.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Creation()
+        {
+            var status = new Status {StatusStage = "Nouveau status"};
+
+            _bd.status.Add(status);
+            _bd.SaveChanges();
+
+            return View("~/Views/Status/Edition.cshtml", status);
         }
     }
 }
