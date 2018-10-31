c82b7f7ef02c1c04a2e1d3896c2e0dd50a901923
MathieuAuclair <mathiass12@hotmail.com>
2018-10-31 11:06:53 -0400

    add Contact feature

--- SysInternshipManagement/Controllers/ContactController.cs
+++ SysInternshipManagement/Controllers/ContactController.cs
@@ -1,21 +1,62 @@
-using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Web.Mvc;
 using SysInternshipManagement.Migrations;
-using SysInternshipManagement.Models;
 
 namespace SysInternshipManagement.Controllers
 {
-    public class ContactController 
+    public class ContactController : Controller
     {
         private readonly DatabaseContext _bd = new DatabaseContext();
 
-        public IEnumerable<Contact> RecolterTousLesContacts()
+        public ActionResult Index()
         {
-            return (
-                from entity
-                    in _bd.contact
-                select entity
-            );
+            return View("~/Views/Contact/Index.cshtml", _bd.contact.ToList());
+        }
+
+        public ActionResult Edition(int? idContact)
+        {
+            if (idContact == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var contact = _bd.contact.Find(idContact);
+
+            if (contact == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View("~/Views/Contact/Edition.cshtml", contact);
+        }
+
+        public ActionResult EnregistrerLesModifications(
+            int? idContact,
+            string nom,
+            string courriel,
+            string telephone
+        )
+        {
+            if (idContact == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var contact = _bd.contact.Find(idContact);
+
+            if (contact == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+
+            contact.Courriel = courriel;
+            contact.Nom = nom;
+            contact.Telephone = telephone;
+
+            _bd.SaveChanges();
+
+            return RedirectToAction("Edition", "Contact");
         }
     }
 }
